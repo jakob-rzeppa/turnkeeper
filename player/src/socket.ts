df592f452bc598d52f70cfc95b407f916a3159f9
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-08-19 23:07:57 +0200

    refactor: simplify socket event handling by using onAny

--- player/src/socket.ts
+++ player/src/socket.ts
@@ -2,10 +2,6 @@ import io from 'socket.io-client';
 
 export const socket = io('http://localhost:3000');
 
-socket.on('connect', () => {
-    console.log('Connected to server');
-});
-
-socket.on('disconnect', () => {
-    console.log('Disconnected from server');
+socket.onAny((event, ...args) => {
+  console.log(event, args);
 });
