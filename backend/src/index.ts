43685b7e676dc3a86ceb6d7ba53ddf6602b6fa66
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-08-19 20:51:15 +0200

    changed from express-websocket to socket.io connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,22 @@
-// index.js
-import config from "#config/config.js";
-import express from "express";
-const app = express();
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+import config from "./config/config.js";
+
 const port = config.port;
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-    console.log("Response sent");
+const httpServer = createServer();
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+io.on("connection", (socket) => {
+  console.log(`Client connected: ${socket.id}`);
+});
+
+httpServer.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
