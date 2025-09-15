b087e5e0e323f54f9090c43cd0863d8c9787f698
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-09-15 17:02:27 +0200

    removed deprecated code in backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,6 @@ import { createServer } from "http";
 import { Server } from "socket.io";
 
 import config from "./config/config.js";
-import gmController from "./controllers/gmController.js";
 
 const port = config.port;
 
@@ -11,15 +10,6 @@ const io = new Server(httpServer, {
     cors: {
         origin: "*",
     },
-});
-
-io.on("connection", (socket) => {
-    const gmConnection = socket.handshake.auth.gmConnection;
-
-    if (gmConnection) {
-        gmController.initConnection(socket);
-        gmController.sendPlayerData();
-    }
 });
 
 httpServer.listen(port, () => {
