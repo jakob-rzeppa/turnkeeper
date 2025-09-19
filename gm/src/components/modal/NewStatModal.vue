5ad7fbbfa10b32f253fac6139649b91200327cf1
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-09-19 08:37:04 +0200

    added logic in NewStatModal to send the event to the backend

--- gm/src/components/modal/NewStatModal.vue
+++ gm/src/components/modal/NewStatModal.vue
@@ -1,15 +1,40 @@
 <script setup lang="ts">
+import type { PlayerStat } from '@/types/player'
+import { socket } from '@/util/connection'
+import { ref } from 'vue'
+
 const props = defineProps<{
     playerId?: string
     playerName?: string
 }>()
+
+const emit = defineEmits(['close'])
+
+const scopeRef = ref('global' as 'global' | 'player')
+const statNameRef = ref('')
+const statInitialValueRef = ref('')
+
+const createStat = () => {
+    const statData: PlayerStat = {
+        name: statNameRef.value,
+        value: statInitialValueRef.value,
+    }
+
+    socket.emit('players:stats:create', {
+        scope: scopeRef.value,
+        playerId: props.playerId,
+        statData,
+    })
+
+    emit('close')
+}
 </script>
 
 <template>
     <h1 class="text-4xl text-center text-primary">Add new stat</h1>
     <label class="select select-primary">
         <span class="label">Scope</span>
-        <select>
+        <select v-model="scopeRef">
             <option value="global" selected>Global</option>
             <option value="player" :disabled="!props.playerId">
                 Player {{ props.playerName ?? '(unknown)' }}
@@ -18,11 +43,11 @@ const props = defineProps<{
     </label>
     <label class="input input-primary">
         <span class="label">Name</span>
-        <input type="text" />
+        <input type="text" v-model="statNameRef" />
     </label>
     <label class="input input-primary">
-        <span class="label">Value</span>
-        <input type="text" />
+        <span class="label">Initial Value</span>
+        <input type="text" v-model="statInitialValueRef" />
     </label>
-    <button class="btn btn-primary">Add Stat</button>
+    <button class="btn btn-primary" @click="createStat">Add Stat</button>
 </template>
