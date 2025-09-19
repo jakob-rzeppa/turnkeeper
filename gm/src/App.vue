64e366c3cbcd9fd96c7faeddcb2341abd8b9620f
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-09-19 19:28:53 +0200

    moved Connection Connect / Disconnect into seperate component

--- gm/src/App.vue
+++ gm/src/App.vue
@@ -1,22 +1,13 @@
 <script setup lang="ts">
-import connectionService, { socket } from './util/connection'
-
-import Login from './components/Login.vue'
 import PlayerDisplay from './components/PlayerDisplay.vue'
 import PlayerListener from './listeners/PlayerListener.vue'
 import ModalController from './components/modal/ModalController.vue'
-
-const isConnected = connectionService.isConnected()
-
-function disconnect() {
-    connectionService.disconnect()
-}
+import ConnectionController from './components/ConnectionController.vue'
 </script>
 
 <template>
-    <Login v-if="!isConnected" />
     <main>
-        <button @click="disconnect">Disconnect</button>
+        <ConnectionController />
         <PlayerDisplay />
         <PlayerListener />
     </main>
---
+++ gm/src/components/ConnectionController.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import connection from '@/util/connection'
+
+const isConnected = connection.isConnected()
+
+function connect() {
+    connection.connect()
+}
+
+function disconnect() {
+    connection.disconnect()
+}
+</script>
+
+<template>
+    <div>
+        <button v-if="!isConnected" class="btn btn-primary btn-sm w-fit" @click="connect">
+            Connect
+        </button>
+        <button v-if="isConnected" class="btn btn-error btn-sm w-fit" @click="disconnect">
+            Disconnect
+        </button>
+    </div>
+</template>
