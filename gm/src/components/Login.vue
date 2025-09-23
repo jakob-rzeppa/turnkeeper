451958d3dcf10808ceae30ad1750f8ed220c6ebc
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-09-23 09:29:37 +0200

    removed deprecated Login component in gm

--- gm/src/components/Login.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-import connectionService from '@/util/connection'
-</script>
-
-<template>
-    <button @click="() => connectionService.connect()">Login</button>
-</template>
