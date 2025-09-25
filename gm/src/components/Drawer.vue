d82198b463d48d1ca5a74535bc2676552f6de9d7
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-09-25 17:56:48 +0200

    fixed elements in drawer showing when closed

--- gm/src/components/Drawer.vue
+++ gm/src/components/Drawer.vue
@@ -1,7 +1,11 @@
 <template>
     <div
-        class="fixed top-0 left-0 z-50 h-screen w-2xs bg-base-200 -translate-x-5/6 hover:translate-x-0 transition-transform p-4 flex flex-col gap-4 shadow-lg"
+        class="fixed top-0 left-0 z-50 h-screen w-2xs bg-base-200 -translate-x-5/6 hover:translate-x-0 transition-transform shadow-lg group"
     >
-        <slot />
+        <div
+            class="w-full h-full p-4 flex flex-col gap-4 opacity-0 group-hover:opacity-100 transition-opacity duration-200"
+        >
+            <slot />
+        </div>
     </div>
 </template>
