bf9c4eba5e1549a4a29c69cfef4b8d4af90fe59a
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-09-26 21:02:23 +0200

    added check when initializing the game to avoid re-initializing the game

--- backend/src/services/gameloop.ts
+++ backend/src/services/gameloop.ts
@@ -1,15 +1,24 @@
 import logger from "./logger.js";
 
-// playerIds in order
-const playerOrder = [] as string[];
-
-const round = {
-    roundNumber: 0,
-    currentPlayerIndex: 0,
+const game = {
+    isInitialized: false,
+    playerOrder: [] as string[], // array of player IDs in turn order
+    round: {
+        roundNumber: 0,
+        currentPlayerIndex: 0,
+    },
 };
 
 export const gameloop = {
     init: (newPlayerOrder: string[]) => {
+        if (game.isInitialized) {
+            logger.error({
+                message:
+                    "Game loop already initialized. Aborting initialization",
+            });
+            return;
+        }
+
         if (newPlayerOrder.length === 0) {
             logger.error({
                 message:
@@ -18,53 +27,54 @@ export const gameloop = {
             return;
         }
 
-        round.roundNumber = 0;
-        round.currentPlayerIndex = 0;
+        game.round.roundNumber = 0;
+        game.round.currentPlayerIndex = 0;
+        game.isInitialized = true;
 
         // Reset and set player order
-        playerOrder.splice(0, playerOrder.length, ...newPlayerOrder);
+        game.playerOrder.splice(0, game.playerOrder.length, ...newPlayerOrder);
 
         logger.info({
             message: "Game loop initialized",
         });
     },
     nextTurn: () => {
-        if (playerOrder.length === 0) return;
+        if (game.playerOrder.length === 0) return;
 
         logger.info({
             message: "End of turn",
             details: {
-                roundNumber: round.roundNumber,
-                playerId: playerOrder[round.currentPlayerIndex],
+                roundNumber: game.round.roundNumber,
+                playerId: game.playerOrder[game.round.currentPlayerIndex],
             },
         });
 
-        round.currentPlayerIndex = round.currentPlayerIndex + 1;
+        game.round.currentPlayerIndex = game.round.currentPlayerIndex + 1;
 
-        if (round.currentPlayerIndex > playerOrder.length - 1) {
-            round.roundNumber += 1;
-            round.currentPlayerIndex = 0;
+        if (game.round.currentPlayerIndex > game.playerOrder.length - 1) {
+            game.round.roundNumber += 1;
+            game.round.currentPlayerIndex = 0;
             logger.info({
                 message: "New round started",
-                details: { roundNumber: round.roundNumber },
+                details: { roundNumber: game.round.roundNumber },
             });
         }
 
         logger.info({
             message: "Start of turn",
             details: {
-                roundNumber: round.roundNumber,
-                playerId: playerOrder[round.currentPlayerIndex],
+                roundNumber: game.round.roundNumber,
+                playerId: game.playerOrder[game.round.currentPlayerIndex],
             },
         });
     },
     getRoundInformation: () => {
         return {
-            roundNumber: round.roundNumber,
-            currentPlayerIndex: round.currentPlayerIndex,
+            roundNumber: game.round.roundNumber,
+            currentPlayerIndex: game.round.currentPlayerIndex,
         };
     },
     getPlayerOrder: () => {
-        return playerOrder;
+        return game.playerOrder;
     },
 };
