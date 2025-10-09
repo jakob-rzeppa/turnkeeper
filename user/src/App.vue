dcb65034de84024ac5e803a7692fac33e8995168
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-10-09 14:26:05 +0200

    refactored user connection client

--- user/src/App.vue
+++ user/src/App.vue
@@ -1,26 +1,9 @@
 <script setup lang="ts">
-import connectionService, { socket } from './util/connection'
-
 import Login from './components/Login.vue'
-
-const isConnected = connectionService.isConnected()
-
-function sendMessage() {
-  socket.send('hi')
-}
-
-function disconnect() {
-  connectionService.disconnect()
-}
 </script>
 
 <template>
-  <Login v-if="!isConnected" />
-  <main>
-    <h1>Hello</h1>
-    <button @click="sendMessage">Send Message</button>
-    <button @click="disconnect">Disconnect</button>
-  </main>
+    <Login />
 </template>
 
 <style scoped></style>
--- user/src/components/Login.vue
+++ user/src/components/Login.vue
@@ -1,21 +1,22 @@
 <script setup lang="ts">
+import useConnection from '@/composables/connection'
 import { ref } from 'vue'
 
-import connectionService from '@/util/connection'
+const connection = useConnection()
 
 const name = ref('')
 const secret = ref('')
 
 function login() {
-  connectionService.connect({ playerName: name.value, playerSecret: secret.value })
+    connection.connect({ playerName: name.value, playerSecret: secret.value })
 }
 </script>
 
 <template>
-  <div>
-    <h1>Login</h1>
-    <input v-model="name" placeholder="Name" />
-    <input v-model="secret" placeholder="Secret" />
-    <button @click="login">Login</button>
-  </div>
+    <div>
+        <h1>Login</h1>
+        <input v-model="name" placeholder="Name" />
+        <input v-model="secret" placeholder="Secret" />
+        <button @click="login">Login</button>
+    </div>
 </template>
