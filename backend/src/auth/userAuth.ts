98d33ad9afd219c6121a925985784157ef44f80d
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-10-09 14:34:07 +0200

    added documentation to userAuth

--- backend/src/auth/userAuth.ts
+++ backend/src/auth/userAuth.ts
@@ -1,5 +1,10 @@
 import playerRepository from "../repositories/playerRepository.js";
-
+/**
+ * The secret is a simple string that is generated when the player is created.
+ * It is not meant to be secure, just to prevent accidental impersonation.
+ *
+ * @returns true if the secret is valid, false otherwise.
+ */
 export const isUserSecretValid = (
     playerId: string,
     providedSecret: string
