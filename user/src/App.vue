b5c51d967818ea51a9743e9b47ee863bb5bdb0dc
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-10-09 19:50:50 +0200

    only display login in user when not connected

--- user/src/App.vue
+++ user/src/App.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import Login from './components/Login.vue'
+import useConnection from './composables/connection'
+
+const { isConnected } = useConnection()
 </script>
 
 <template>
-    <Login />
+    <Login v-if="!isConnected" />
 </template>
 
 <style scoped></style>
