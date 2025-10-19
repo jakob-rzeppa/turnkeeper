570a47ceb0a93e9c4ae9c304df814df6e5f4f390
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-10-19 12:19:57 +0200

    gm: allowed creating of stats with no value

--- gm/src/components/player/NewStatModal.vue
+++ gm/src/components/player/NewStatModal.vue
@@ -78,7 +78,7 @@ const createStat = () => {
         <button
             class="btn btn-primary btn-lg w-full"
             @click="createStat"
-            :disabled="!statNameRef.trim() || !statInitialValueRef.trim()"
+            :disabled="!statNameRef.trim()"
         >
             <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path
