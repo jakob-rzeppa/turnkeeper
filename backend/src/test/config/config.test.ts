7a197079758115c1beafb9acd8bab4bf7919f1d6
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-10-25 10:30:11 +0200

    backend: created config tests

---
+++ backend/src/test/config/config.test.ts
@@ -0,0 +1,65 @@
+import { describe, it, expect, vi, beforeEach } from 'vitest';
+
+vi.mock('dotenv', () => ({
+    default: {
+        config: vi.fn(),
+    },
+}));
+
+// dynamic imports and vi.resetModules are used to ensure fresh module state for each test
+describe('Config', () => {
+    beforeEach(() => {
+        vi.resetModules();
+        vi.clearAllMocks();
+    });
+
+    describe('Config initialization', () => {
+        it('should initialize with process.env values', async () => {
+            process.env.DB_PATH = 'f';
+            process.env.PORT = '200';
+
+            const { default: config } = await import('../../config/config');
+
+            expect(config.port).toBe(200);
+            expect(config.dbPath).toBe('f');
+        });
+
+        it('should initialize with default values', async () => {
+            delete process.env.DB_PATH;
+            delete process.env.PORT;
+
+            const { default: config } = await import('../../config/config');
+
+            expect(config.port).toBe(3000);
+            expect(config.dbPath).toBeNull();
+        });
+    });
+
+    describe('Config structure', () => {
+        it('should have exactly two properties', async () => {
+            const { default: config } = await import('../../config/config');
+            const keys = Object.keys(config);
+            expect(keys).toHaveLength(2);
+            expect(keys).toContain('dbPath');
+            expect(keys).toContain('port');
+        });
+
+        it('should not allow modifications', async () => {
+            const { default: config } = await import('../../config/config');
+            const initialKeys = Object.keys(config);
+
+            expect(() => ((config as any).newProp = 'test')).toThrowError(
+                new TypeError('Cannot add property newProp, object is not extensible'),
+            );
+            expect(() => ((config as any).dbPath = 'test')).toThrowError(
+                new TypeError("Cannot assign to read only property 'dbPath' of object '#<Object>'"),
+            );
+            expect(() => ((config as any).port = 3000)).toThrowError(
+                new TypeError("Cannot assign to read only property 'port' of object '#<Object>'"),
+            );
+
+            expect(Object.keys(config)).toHaveLength(initialKeys.length);
+            expect(Object.keys(config)).toEqual(initialKeys);
+        });
+    });
+});
