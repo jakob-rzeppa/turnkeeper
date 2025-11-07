d48609bf4b7d40030135a5843c4256f89603ac1f
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-11-07 07:38:01 +0100

    gm: fixed .env variables not loaded

--- gm/src/composables/useConnection.ts
+++ gm/src/composables/useConnection.ts
@@ -1,7 +1,7 @@
 import io from 'socket.io-client';
 import { ref } from 'vue';
 
-const socket = io(import.meta.env.BACKEND_URL || 'http://localhost:3000/gm', {
+const socket = io(import.meta.env.VITE_BACKEND_URL, {
     autoConnect: true,
 });
 const isConnected = ref(socket.connected);
