d8c676818e9d5cb65a62c12c0463aea135af1289
jakob-rzeppa <rzeppa.jakob@gmail.com>
2026-01-02 00:08:48 +0100

    implement new message system for gm client

--- gm/src/stores/messagesStore.ts
+++ gm/src/stores/messagesStore.ts
@@ -3,19 +3,40 @@ import { defineStore } from 'pinia';
 import type { BackendToGmEventPayloads, Message } from 'shared-types';
 import { ref } from 'vue';
 import { useToastStore } from './toastStore';
+import { usePlayerStore } from './playerStore';
 
 const connection = useConnection();
 
 export const useMessagesStore = defineStore('messages', () => {
     const messages = ref<{ [key: number]: Message[] }>({});
     const toastStore = useToastStore();
+    const playerStore = usePlayerStore();
 
     connection.socket.on(
         'messages:all',
         ({ messages: newMessages }: BackendToGmEventPayloads['messages:all']) => {
-            toastStore.addToast('New messages received', 'info', 4000);
-
             messages.value = newMessages;
+        },
+    );
+
+    connection.socket.on(
+        'messages:new',
+        ({ message }: BackendToGmEventPayloads['messages:new']) => {
+            if (message.sendBy === 'system') {
+                toastStore.addToast('New message from system', 'info', 5000);
+            } else if (message.sendBy === 'player') {
+                toastStore.addToast(
+                    'New message from ' + playerStore.getPlayerById(message.playerId)?.name ||
+                        'a player',
+                    'info',
+                    5000,
+                );
+            }
+
+            if (!messages.value[message.playerId]) {
+                messages.value[message.playerId] = [];
+            }
+            messages.value[message.playerId].push(message);
         },
     );
 
