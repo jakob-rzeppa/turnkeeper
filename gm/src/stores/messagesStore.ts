a1cb5ef730851038d538219d525e842ddb323895
jakob-rzeppa <rzeppa.jakob@gmail.com>
2026-01-01 22:37:54 +0100

    add toast if new message is received

--- gm/src/stores/messagesStore.ts
+++ gm/src/stores/messagesStore.ts
@@ -2,15 +2,19 @@ import useConnection from '@/composables/useConnection';
 import { defineStore } from 'pinia';
 import type { BackendToGmEventPayloads, Message } from 'shared-types';
 import { ref } from 'vue';
+import { useToastStore } from './toastStore';
 
 const connection = useConnection();
 
 export const useMessagesStore = defineStore('messages', () => {
     const messages = ref<{ [key: number]: Message[] }>({});
+    const toastStore = useToastStore();
 
     connection.socket.on(
         'messages:all',
         ({ messages: newMessages }: BackendToGmEventPayloads['messages:all']) => {
+            toastStore.addToast('New messages received', 'info', 4000);
+
             messages.value = newMessages;
         },
     );
