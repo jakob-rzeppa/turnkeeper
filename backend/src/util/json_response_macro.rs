8e7e32519b3e60b21593b813b2e3da6c44471ea1
jakob-rzeppa <rzeppa.jakob@gmail.com>
2026-01-03 22:46:41 +0100

    update json_response_macro to include struct definition

--- backend/src/util/json_response_macro.rs
+++ backend/src/util/json_response_macro.rs
@@ -1,7 +1,10 @@
 
 /// Implements .into_response for the struct with a StatusCode::Ok
 #[macro_export] macro_rules! json_response {
-    ($struct_name:ident) => {
+    ($struct_name:ident, $struct:tt) => {
+        #[derive(Serialize)]
+        pub struct $struct_name $struct
+
         impl IntoResponse for $struct_name {
             fn into_response(self) -> axum::response::Response {
                 (StatusCode::OK, Json(self)).into_response()
