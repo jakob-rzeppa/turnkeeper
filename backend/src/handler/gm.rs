f3b81545376dce7a72521ad88f23b1fda0c1dd90
jakob-rzeppa <rzeppa.jakob@gmail.com>
2026-01-03 22:47:03 +0100

    define Response types of gm and user routes

--- backend/src/handler/gm.rs
+++ backend/src/handler/gm.rs
@@ -1,11 +1,18 @@
+use axum::http::StatusCode;
 use axum::Json;
-use serde_json::Value;
+use axum::response::IntoResponse;
+use serde::{Serialize};
 use crate::error::HttpError;
+use crate::json_response;
+
+json_response!(LoginResponse, {
+    token: String,
+});
 
 /// POST /gm/login
 ///
 /// authenticates the gm via a secret set in the environment variables
 /// and returns a JSON WEB TOKEN
-pub async fn login() -> Result<Json<Value>, HttpError> {
+pub async fn login() -> Result<LoginResponse, HttpError> {
     Err(HttpError::NotImplemented)
 }
--- backend/src/handler/user.rs
+++ backend/src/handler/user.rs
@@ -1,17 +1,28 @@
+use axum::response::IntoResponse;
+use axum::http::StatusCode;
+use serde::Serialize;
 use axum::Json;
-use serde_json::Value;
 use crate::error::HttpError;
+use crate::json_response;
+
+json_response!(LoginResponse, {
+    token: String,
+});
 
 /// POST /user/login
 ///
 /// authenticates a user via username and password and returns a JSON WEB TOKEN
-pub async fn login() -> Result<Json<Value>, HttpError> {
+pub async fn login() -> Result<LoginResponse, HttpError> {
     Err(HttpError::NotImplemented)
 }
+
+json_response!(RegisterResponse, {
+    token: String,
+});
 
 /// POST /user/register
 ///
 /// registers a new user via username and password
-pub async fn register() -> Result<Json<Value>, HttpError> {
+pub async fn register() -> Result<RegisterResponse, HttpError> {
     Err(HttpError::NotImplemented)
 }
