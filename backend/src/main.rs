8849942cce3d9ea1d437a2b025122e2f2b0793c2
jakob-rzeppa <rzeppa.jakob@gmail.com>
2026-01-03 18:27:48 +0100

    create simple http server return Hello World

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    // Initialize the Axum router
+    let app = Router::new().route("/", get(root_handler));
+
+    // Specify the address to bind to
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+
+    let listener = TcpListener::bind(addr).await.expect("create tcp listener failed");
+
+    // Start the Axum server
+    axum::serve(listener, app).await.expect("launch server failed");
+    
+    println!("Server running at {}", addr);
+}
+
+async fn root_handler() -> &'static str {
+    "Hello, world!"
 }
