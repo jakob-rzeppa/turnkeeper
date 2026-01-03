a55b1bfeac87ba12d481dc2f77f67aa16b077ed8
jakob-rzeppa <rzeppa.jakob@gmail.com>
2026-01-03 21:53:19 +0100

    create game http routes

--- backend/src/handler/game.rs
+++ backend/src/handler/game.rs
@@ -2,6 +2,30 @@ use axum::Json;
 use serde_json::Value;
 use crate::error::HttpError;
 
-pub async fn create_game() -> Result<Json<Value>, HttpError> {
+/// GET /games
+///
+/// returns a list of all available games
+pub async fn games_get_all() -> Result<Json<Value>, HttpError> {
+    Err(HttpError::NotImplemented)
+}
+
+/// GET /games/:id
+///
+/// returns the game state of the game with given id
+pub async fn games_get() -> Result<Json<Value>, HttpError> {
+    Err(HttpError::NotImplemented)
+}
+
+/// POST /games
+///
+/// creates a game and returns the initial game state
+pub async fn games_create() -> Result<Json<Value>, HttpError> {
+    Err(HttpError::NotImplemented)
+}
+
+/// DELETE /games
+///
+/// deletes a game if no current connection to it
+pub async fn games_delete() -> Result<Json<Value>, HttpError> {
     Err(HttpError::NotImplemented)
 }
--- backend/src/handler/mod.rs
+++ backend/src/handler/mod.rs
@@ -1,10 +1,13 @@
 mod game;
 
 use axum::Router;
-use axum::routing::{get, post};
-use crate::handler::game::create_game;
+use axum::routing::{delete, get, post};
+use crate::handler::game::{games_create, games_delete, games_get, games_get_all};
 
 pub fn get_routes() -> Router {
     Router::new()
-        .route("/game", get(create_game))
+        .route("/games", get(games_get_all))
+        .route("/games/:id", get(games_get))
+        .route("/games", post(games_create))
+        .route("/games/:id", delete(games_delete))
 }
