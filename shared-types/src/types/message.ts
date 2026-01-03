b8023eb1ee97562ddfa28c9113af9a2d665cff5e
jakob-rzeppa <rzeppa.jakob@gmail.com>
2026-01-03 22:13:53 +0100

    delete old files

--- shared-types/src/types/message.ts
+++
@@ -1,7 +0,0 @@
-export interface Message {
-    id: number;
-    playerId: number;
-    sendBy: "player" | "system" | "gm";
-    content: string;
-    timestamp: Date;
-}
--- user/src/components/PlayerNameDisplay.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import { usePlayerStore } from '@/stores/playerStore';
-
-const { player } = usePlayerStore();
-</script>
-
-<template>
-    <h1 v-if="player" class="text-3xl text-primary text-center font-bold">{{ player.name }}</h1>
-</template>
