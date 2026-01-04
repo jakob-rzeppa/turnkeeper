30b9b54bdafa7e819fc060254c19af410b03648e
jakob-rzeppa <rzeppa.jakob@gmail.com>
2026-01-04 22:29:49 +0100

    remove automatic run of migrations on startup

--- backend/src/db/mod.rs
+++ backend/src/db/mod.rs
@@ -1,6 +1,5 @@
 use sqlx::sqlite::{SqliteConnectOptions, SqlitePool, SqlitePoolOptions};
 use std::str::FromStr;
-use sqlx::migrate;
 
 pub async fn create_pool(database_url: &str) -> Result<SqlitePool, sqlx::Error> {
     let options = SqliteConnectOptions::from_str(database_url)?
@@ -10,11 +9,6 @@ pub async fn create_pool(database_url: &str) -> Result<SqlitePool, sqlx::Error> 
     let pool = SqlitePoolOptions::new()
         .max_connections(10)
         .connect_with(options)
-        .await?;
-
-    // Run migrations
-    migrate!("src/migrations")
-        .run(&pool)
         .await?;
 
     Ok(pool)
