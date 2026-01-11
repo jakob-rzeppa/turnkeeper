34607de9cdb334c439595cded562fd9c22f6396b
jakob-rzeppa <rzeppa.jakob@gmail.com>
2026-01-11 17:45:05 +0100

    init user client and update gm client to have the same config as user

--- backend/src/handler/mod.rs
+++ backend/src/handler/mod.rs
@@ -1,6 +1,6 @@
-mod game;
-mod user;
-mod gm;
+pub mod game;
+pub mod user;
+pub mod gm;
 
 use axum::Router;
 use axum::routing::{delete, get, post};
@@ -12,9 +12,9 @@ use crate::handler::user::{login as login_user, register as register_user};
 pub fn get_routes() -> Router<AppState> {
     Router::new()
         .route("/games", get(games_get_all))
-        .route("/games/:id", get(games_get))
+        .route("/games/{id}", get(games_get))
         .route("/games", post(games_create))
-        .route("/games/:id", delete(games_delete))
+        .route("/games/{id}", delete(games_delete))
 
         .route("/user/login", post(login_user))
         .route("/user/register", post(register_user))
