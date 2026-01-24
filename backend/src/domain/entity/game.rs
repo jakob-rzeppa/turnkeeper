f90296bfb33751e7250e7a0b843cef16cb3b40fc
jakob-rzeppa <rzeppa.jakob@gmail.com>
2026-01-24 09:11:13 +0100

    remove GameBuilder and change Game id to Uuid

--- backend/src/domain/entity/game.rs
+++ backend/src/domain/entity/game.rs
@@ -1,15 +1,15 @@
 use uuid::Uuid;
 use crate::domain::entity::player::Player;
-use crate::domain::value_object::identifier::Identifier;
+use crate::domain::entity::user::User;
 
 /// The representation of the game
-/// 
+///
 /// # Creation
-/// 
+///
 /// - For a new Game use `Game::new(id: Uuid)`.
 /// - When instantiating a existing Game using `Game::builder()` is recommended.
 pub struct Game {
-    id: Identifier,
+    id: Uuid,
 
     players: Vec<Player>,
 
@@ -19,8 +19,6 @@ pub struct Game {
 
 impl Game {
     pub fn new(id: Uuid) -> Self {
-        let id = Identifier::new(id);
-        
         Self {
             id,
             players: Vec::new(),
@@ -28,20 +26,4 @@ impl Game {
             current_player_index: 0,
         }
     }
-    
-    pub fn builder() -> GameBuilder {
-        GameBuilder::default()
-    }
-}
-
-#[derive(Default)]
-pub struct GameBuilder {
-    id: Option<Identifier>,
-    players: Vec<Player>,
-
-    round_number: Option<u32>,
-    current_player_index: Option<usize>,
-}
-
-impl GameBuilder {
 }
