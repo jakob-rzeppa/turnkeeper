418205fb67f9dfed1767c44f6e65c93f367c883c
jakob-rzeppa <rzeppa.jakob@gmail.com>
2026-01-24 09:10:23 +0100

    move id and key from StatKind enum to Stat struct

--- backend/src/domain/entity/stat.rs
+++ backend/src/domain/entity/stat.rs
@@ -1,5 +1,4 @@
 use uuid::Uuid;
-use crate::domain::value_object::identifier::Identifier;
 use crate::domain::value_object::stat::key::Key;
 use crate::domain::value_object::stat::boolean_value::BooleanValue;
 use crate::domain::value_object::stat::number_value::NumberValue;
@@ -7,64 +6,65 @@ use crate::domain::value_object::stat::string_value::StringValue;
 use crate::error::DomainError;
 
 /// The representation of a stat
-/// 
+///
 /// Stat kinds are
 ///
 /// 1. Number
 /// 2. String
 /// 3. Boolean
-/// 
+///
 /// Use `try_new_string_stat`, `new_number_stat` or `new_bool_stat` for instantiating the Stat.
+#[derive(Debug, Clone, PartialEq)]
 pub struct Stat {
+    id: Uuid,
+    key: Key,
     kind: StatKind,
 }
 
+#[derive(Debug, Clone, PartialEq)]
 enum StatKind {
     Number {
-        id: Identifier,
-        key: Key,
         value: NumberValue,
     },
     String {
-        id: Identifier,
-        key: Key,
         value: StringValue,
     },
     Boolean {
-        id: Identifier,
-        key: Key,
         value: BooleanValue,
     },
 }
 
 impl Stat {
     pub fn try_new_string_stat(id: Uuid, key: String, value: String) -> Result<Self, DomainError> {
-        let id = Identifier::new(id);
-        let key = Key::try_new(key)?;
+        let key = Key::try_new(key).map_err(|e| e.prefix("new string stat".to_string()))?;
         let value = StringValue::new(value);
 
         Ok(Self {
-            kind: StatKind::String { id, key, value }
+            id,
+            key,
+            kind: StatKind::String { value }
         })
     }
 
-    pub fn new_number_stat(id: Uuid, key: String, value: i64) -> Result<Self, DomainError> {
-        let id = Identifier::new(id);
-        let key = Key::try_new(key)?;
+    pub fn try_new_number_stat(id: Uuid, key: String, value: i64) -> Result<Self, DomainError> {
+        let key = Key::try_new(key).map_err(|e| e.prefix("new number stat".to_string()))?;
         let value = NumberValue::new(value);
 
         Ok(Self {
-            kind: StatKind::Number { id, key, value }
+            id,
+            key,
+            kind: StatKind::Number { value }
         })
     }
 
-    pub fn new_bool_stat(id: Uuid, key: String, value: bool) -> Result<Self, DomainError> {
-        let id = Identifier::new(id);
-        let key = Key::try_new(key)?;
+    pub fn try_new_bool_stat(id: Uuid, key: String, value: bool) -> Result<Self, DomainError> {
+        let key = Key::try_new(key).map_err(|e| e.prefix("new bool stat".to_string()))?;
         let value = BooleanValue::new(value);
 
         Ok(Self {
-            kind: StatKind::Boolean { id, key, value }
+            id,
+            key,
+            kind: StatKind::Boolean { value }
         })
     }
 }
