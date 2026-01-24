dca86533eb951b5ebe4b8501a61c5102bf220ef0
jakob-rzeppa <rzeppa.jakob@gmail.com>
2026-01-24 09:10:00 +0100

    update user to use Uuid and enhance error messages

--- backend/src/domain/entity/user.rs
+++ backend/src/domain/entity/user.rs
@@ -1,12 +1,11 @@
 use uuid::Uuid;
-use crate::domain::value_object::identifier::Identifier;
 use crate::domain::value_object::name::Name;
 use crate::domain::value_object::password::Password;
 use crate::error::DomainError;
 
 /// The representation of a user
 pub struct User {
-    id: Identifier,
+    id: Uuid,
     name: Name,
     // the password is stored in plain text,
     // so the gm can look up a password if a user forgot it
@@ -15,9 +14,8 @@ pub struct User {
 
 impl User {
     pub fn try_new(id: Uuid, name: String, password: String) -> Result<Self, DomainError> {
-        let id = Identifier::new(id);
-        let name = Name::try_new(name)?;
-        let password = Password::try_new(password)?;
+        let name = Name::try_new(name).map_err(|e| e.prefix("new user".to_string()))?;
+        let password = Password::try_new(password).map_err(|e| e.prefix("new user".to_string()))?;
         
         Ok(Self { id, name, password })
     }
