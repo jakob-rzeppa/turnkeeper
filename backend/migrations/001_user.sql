1a0ecb1ba9c52604eb223e383ff21f04115b111b
jakob-rzeppa <rzeppa.jakob@gmail.com>
2026-01-25 22:35:26 +0100

    update 001_user.sql migration to represent the new user

--- backend/migrations/001_user.sql
+++ backend/migrations/001_user.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT UNIQUE NOT NULL,
-    password TEXT NOT NULL
+    id                  VARCHAR(36)     UNIQUE NOT NULL,
+    name                VARCHAR(255)    UNIQUE NOT NULL,
+    password            VARCHAR(255)    NOT NULL
 )
