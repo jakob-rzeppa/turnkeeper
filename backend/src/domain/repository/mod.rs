61595ee5449df4218acef8ffd312b07b75e9a88b
jakob-rzeppa <rzeppa.jakob@gmail.com>
2026-01-25 09:54:11 +0100

    change the Error of UserRepositoryTrait and GameRepositoryTrait to the standart domain Error

--- backend/src/domain/repository/mod.rs
+++ backend/src/domain/repository/mod.rs
@@ -1,18 +1,20 @@
 use uuid::Uuid;
 use crate::domain::entity::game::Game;
 use crate::domain::entity::user::User;
-use crate::error::DomainError;
+use crate::domain::error::Error;
 
+#[mockall::automock]
 pub trait GameRepositoryTrait {
-    fn get_by_id(&self, game_id: Uuid) -> Result<Game, DomainError>;
-    fn save(&self, game: Game) -> Result<(), DomainError>;
-    fn update(&self, game: Game) -> Result<(), DomainError>;
-    fn delete(&self, game_id: Uuid) -> Result<(), DomainError>;
+    fn get_by_id(&self, game_id: Uuid) -> Result<Game, Error>;
+    fn save(&self, game: Game) -> Result<(), Error>;
+    fn update(&self, game: Game) -> Result<(), Error>;
+    fn delete(&self, game_id: Uuid) -> Result<(), Error>;
 }
 
+#[mockall::automock]
 pub trait UserRepositoryTrait {
-    fn get_by_id(&self, user_id: Uuid) -> Result<Game, DomainError>;
-    fn save(&self, user: User) -> Result<(), DomainError>;
-    fn update(&self, game: Game) -> Result<(), DomainError>;
-    fn delete(&self, user_id: Uuid) -> Result<(), DomainError>;
+    fn get_by_id(&self, user_id: Uuid) -> Result<User, Error>;
+    fn save(&self, user: User) -> Result<(), Error>;
+    fn update(&self, user: User) -> Result<(), Error>;
+    fn delete(&self, user_id: Uuid) -> Result<(), Error>;
 }
