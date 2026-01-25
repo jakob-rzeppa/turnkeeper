9be3fb2391066bacbf05b5d9e9fcf2fd5ad4451b
jakob-rzeppa <rzeppa.jakob@gmail.com>
2026-01-25 19:09:47 +0100

    use GmAuthHandler in login HttpHandler

--- backend/src/infrastructure/http_handler/gm.rs
+++ backend/src/infrastructure/http_handler/gm.rs
@@ -1,17 +1,10 @@
 use crate::infrastructure::error::HttpError;
-use std::sync::LazyLock;
 use backend_derive::{JsonRequest, JsonResponse};
 use serde::{Deserialize, Serialize};
 use serde_valid::Validate;
-
-const GM_PASSWORD: LazyLock<String> = LazyLock::new(|| {
-    if cfg!(test) {
-        return "test".to_string();
-    }
-
-    std::env::var("GM_PASSWORD")
-        .expect("GM_PASSWORD environment variable is not set")
-});
+use crate::application::auth::dto::LoginGmRequestDto;
+use crate::application::auth::gm_handler::GmAuthHandler;
+use crate::infrastructure::auth::jwt::{JwtGenerator, JwtValidator};
 
 #[derive(Deserialize, Validate, JsonRequest, Debug)]
 pub struct LoginRequest {
@@ -28,5 +21,10 @@ pub struct LoginResponse {
 /// authenticates the gm via a secret set in the environment variables
 /// and returns a JSON WEB TOKEN
 pub async fn login(request: LoginRequest) -> Result<LoginResponse, HttpError> {
-    Err(HttpError::NotImplemented)
+    let gm_auth_handler = GmAuthHandler::new(JwtGenerator {}, JwtValidator {});
+
+    let request_dto = LoginGmRequestDto { password: request.password };
+    let response = gm_auth_handler.login(request_dto)?;
+
+    Ok(LoginResponse{ token: response.token })
 }
