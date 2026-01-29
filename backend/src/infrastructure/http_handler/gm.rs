b04ce0311db9288e2253493433683c5cada0e3dc
jakob-rzeppa <rzeppa.jakob@gmail.com>
2026-01-29 12:41:20 +0100

    pass /gm/login request to GmLoginRequestHandler

--- backend/src/infrastructure/http_handler/gm.rs
+++ backend/src/infrastructure/http_handler/gm.rs
@@ -1,6 +1,9 @@
 use crate::infrastructure::error::HttpError;
 use backend_derive::{JsonRequest, JsonResponse};
 use serde::{Deserialize, Serialize};
+use crate::application::gm::request_handlers::login::GmLoginRequestHandler;
+use crate::application::gm::requests::{GmLoginRequest};
+use crate::infrastructure::auth::gm_jwt::GmJwtGenerator;
 
 #[derive(Deserialize, JsonRequest, Debug)]
 pub struct LoginRequest {
@@ -11,16 +14,20 @@ pub struct LoginRequest {
 pub struct LoginResponse {
     token: String,
 }
+
 /// POST /gm/login
 ///
 /// authenticates the gm via a secret set in the environment variables
 /// and returns a JSON WEB TOKEN
 pub async fn login(request: LoginRequest) -> Result<LoginResponse, HttpError> {
-    // let gm_auth_handler = AuthenticateRequestHandler::new(JwtGenerator::new(), JwtValidator::new());
-    //
-    // let request_dto = LoginGmRequestDto { password: request.password };
-    // let response = gm_auth_handler.login(request_dto)?;
-    //
-    // Ok(LoginResponse{ token: response.token })
-    Err(HttpError::NotImplemented)
+    let gm_auth_handler = GmLoginRequestHandler::new(GmJwtGenerator::new());
+
+    let request = GmLoginRequest {
+        password: request.password,
+    };
+    let result = gm_auth_handler.login(request).await?;
+
+    Ok(LoginResponse {
+        token: result.token,
+    })
 }
