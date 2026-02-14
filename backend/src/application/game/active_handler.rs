4919f995aa5105958550f76580503a9bd0b2f808
jakob-rzeppa <rzeppa.jakob@gmail.com>
2026-02-14 11:21:43 +0100

    delete unused handlers

--- backend/src/application/game/active_handler.rs
+++
@@ -1,8 +0,0 @@
-use crate::domain::game::entities::game::Game;
-
-/// The GameHandler is the manager of the game during the game.
-///
-/// It will be created when starting / resuming a game and is persisted while the game is running.
-pub struct ActiveGameHandler {
-    instance: Game,
-}
