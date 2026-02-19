a39f3dc89e5c3ee156ecd90aecc3aa5951cf34de
jakob-rzeppa <rzeppa.jakob@gmail.com>
2026-02-19 21:47:11 +0100

    add handle_event logic to session

--- backend/src/application/game/session.rs
+++ backend/src/application/game/session.rs
@@ -4,6 +4,7 @@ use crate::application::game::contracts::{GameRepositoryContract, GmConnectionCo
 use crate::application::game::dto::ConnectionMessageDto;
 use crate::domain::game::entities::game::Game;
 use crate::domain::game::error::GameError;
+use crate::domain::game::events::GameEvent;
 use crate::domain::game::projections::GmGameInfo;
 
 pub struct GameSession<GmConnection, GameRepository>
@@ -33,18 +34,37 @@ where
         })
     }
 
+    async fn handle_event(&mut self, event: GameEvent) {
+        let res = self.game.handle_event(event.clone());
+
+        if res.is_ok() {
+            // Persist the game state only if the event was handled successfully
+            // if let Err(e) = self.game_repo.log_event(event).await {
+            //     eprintln!("Failed to save game state: {}", e);
+            // }
+        } else {
+            // TODO: Send error to gm
+            eprintln!("Failed to handle event: {}", res.err().unwrap());
+        }
+
+        self.broadcast_game_state().await;
+    }
+
+    async fn broadcast_game_state(&mut self) {
+        if let Some(gm_conn) = &mut self.gm_conn {
+            match serde_json::to_string(&GmGameInfo::from(&self.game)) {
+                Ok(json) => gm_conn.send(json).await,
+                Err(e) => eprintln!("failed to serialize GmGameInfo: {}", e),
+            }
+        }
+    }
+
     pub async fn gm_connect(&mut self, gm_conn: GmConnection) {
         println!("Gm connection established");
         self.gm_conn = Some(gm_conn);
 
         while let ConnectionMessageDto::Event(event) = self.gm_conn.as_mut().expect("gm_conn is some").recv().await {
-            //self.game.handle(event);
-
-            // Send game to gm
-            match serde_json::to_string(&GmGameInfo::from(&self.game)) {
-                Ok(json) => self.gm_conn.as_mut().expect("gm_conn is some").send(json).await,
-                Err(e) => eprintln!("failed to serialize GmGameInfo: {}", e),
-            }
+            self.handle_event(event).await;
         }
 
         println!("Closing GmWebSocket connection.");
