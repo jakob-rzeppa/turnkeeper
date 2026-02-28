b139d9e63fe0b4d9cd79faa90d11bf5eda721706
jakob-rzeppa <rzeppa.jakob@gmail.com>
2026-02-28 23:45:56 +0100

    remove unused GameOverviewDto struct from dto.rs

--- backend/src/application/game/dto.rs
+++ backend/src/application/game/dto.rs
@@ -1,10 +1,4 @@
-use uuid::Uuid;
 use crate::domain::game::events::GameEvent;
-
-pub struct GameOverviewDto {
-    id: Uuid,
-    name: String,
-}
 
 #[derive(Debug)]
 pub enum ConnectionMessageDto {
