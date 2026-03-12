a49cc87b6c9214931ed487be978d55cefeacaeba
jakob-rzeppa <rzeppa.jakob@gmail.com>
2026-03-12 23:53:06 +0100

    remove autosaveObject

--- gm/src/game/notes/NotesEditor.vue
+++ gm/src/game/notes/NotesEditor.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { useAutosaveObject } from '../../common/autosaveObject';
+import { ref } from 'vue';
 import { useEventEmitter } from '../../events/useEventEmitter';
 import { type Game } from '../gameStore';
 
@@ -9,46 +9,33 @@ const props = defineProps<{
 
 const eventEmitter = useEventEmitter();
 
-const autosave = useAutosaveObject<{
-    notes: string;
-    hiddenNotes: string;
-}>(
-    () => {
-        return {
-            notes: props.game.notes,
-            hiddenNotes: props.game.hiddenNotes,
-        };
-    },
-    newObject => {
-        eventEmitter.setNotes(newObject.notes);
-        eventEmitter.setHiddenNotes(newObject.hiddenNotes);
-    }
-);
+const notes = ref<string>(props.game.notes);
+const hiddenNotes = ref<string>(props.game.hiddenNotes);
 </script>
 
 <template>
     <!-- use a daisyUI card container for visual grouping -->
-    <div class="w-full" @focusout="autosave.saveChanges">
+    <div class="w-full">
         <div class="flex flex-col gap-2">
-            <h2 class="card-title">
-                {{ `Game Notes${autosave.isEditableObjectChanged.value ? '*' : ''}` }}
-            </h2>
+            <h2 class="card-title">Game Notes</h2>
 
             <fieldset class="fieldset">
                 <legend class="fieldset-legend">Game Notes</legend>
                 <textarea
-                    v-model="autosave.editableObject.value.notes"
+                    v-model="notes"
                     class="textarea textarea-bordered h-[40vh] w-full"
                     placeholder="Enter your notes here..."
+                    @change="() => eventEmitter.setNotes(notes)"
                 ></textarea>
             </fieldset>
 
             <fieldset class="fieldset">
                 <legend class="fieldset-legend">Hidden Game Notes</legend>
                 <textarea
-                    v-model="autosave.editableObject.value.hiddenNotes"
+                    v-model="hiddenNotes"
                     class="textarea textarea-bordered h-[30vh] w-full"
                     placeholder="Enter your hidden notes here..."
+                    @change="() => eventEmitter.setHiddenNotes(hiddenNotes)"
                 ></textarea>
             </fieldset>
         </div>
