b244ec5c7e7bfbf9f7f873dd547575b439ba95bc
jakob-rzeppa <rzeppa.jakob@gmail.com>
2026-03-13 21:27:06 +0100

    install devtools

--- user/vite.config.ts
+++ user/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import tailwindcss from '@tailwindcss/vite';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 export default defineConfig({
-    plugins: [vue(), tailwindcss()],
+    plugins: [vue(), tailwindcss(), vueDevTools()],
 });
