06ac38c104e805022b31f3f736955bbc11254c2f
jakob-rzeppa <rzeppa.jakob@gmail.com>
2026-05-04 20:41:34 +0200

    add action executor

--- backend/src/application/game_instance/action_interpreter/error.rs
+++ backend/src/application/game_instance/action_interpreter/error.rs
@@ -1,1 +1,5 @@
-
+#[derive(Debug, thiserror::Error)]
+pub enum ActionInterpreterError {
+    #[error("Action not found: {0}")]
+    ActionNotFound(String),
+}
--- backend/src/application/game_instance/action_interpreter/mod.rs
+++ backend/src/application/game_instance/action_interpreter/mod.rs
@@ -1,3 +1,29 @@
+use crate::{
+    application::game_instance::action_interpreter::error::ActionInterpreterError,
+    domain::game::entities::{game_instance::GameInstance, weak::action::Action},
+};
+
 pub mod error;
-pub mod executor;
-pub mod parser;
+pub struct ActionExecutor {
+    game_instance: GameInstance,
+    action: Action,
+}
+
+impl ActionExecutor {
+    pub fn new(
+        game_instance: GameInstance,
+        action_name: &str,
+    ) -> Result<Self, ActionInterpreterError> {
+        let action = game_instance
+            .actions()
+            .iter()
+            .find(|a| a.name() == action_name)
+            .cloned()
+            .ok_or_else(|| ActionInterpreterError::ActionNotFound(action_name.to_string()))?;
+
+        Ok(Self {
+            game_instance,
+            action,
+        })
+    }
+}
