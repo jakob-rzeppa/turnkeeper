c17b3b1da796d464a0f35971af791cb74346b636
jakob-rzeppa <rzeppa.jakob@gmail.com>
2026-05-08 11:44:15 +0200

    set cors to not any

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -4,16 +4,17 @@ pub mod infrastructure;
 pub mod util;
 
 use crate::infrastructure::{app_state::AppState, http::get_routes, logger::logging_middleware, websocket::get_websocket_routes};
-use axum::{Router, middleware};
+use axum::{Router, http::HeaderValue, middleware};
 use tower::ServiceBuilder;
-use tower_http::cors::{Any, CorsLayer};
+use tower_http::cors::CorsLayer;
+use axum::http::{Method, header::{AUTHORIZATION}};
 
 /// Builds the Axum application router with all routes configured.
 pub fn build_app(state: AppState) -> Router {
     let cors_layer = CorsLayer::new()
-        .allow_origin(Any)
-        .allow_methods(Any)
-        .allow_headers(Any);
+        .allow_origin(std::env::var("CORS_ORIGIN").expect("CORS_ORIGIN environment variable not set.").parse::<HeaderValue>().unwrap())
+        .allow_methods([Method::GET, Method::POST, Method::PUT, Method::DELETE, Method::OPTIONS])
+        .allow_headers([AUTHORIZATION]);
 
     Router::new()
         .merge(get_routes(state.clone()))
